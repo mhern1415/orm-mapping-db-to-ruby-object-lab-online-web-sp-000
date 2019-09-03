add5f582ba9722188ce8e4651a745a39a2871c72
Mark Hernandez <m.hern1415@gmail.com>
2019-09-03 22:52:13 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -19,8 +19,9 @@ class Student
     sql = <<-SQL
     SELECT * 
     FROM students
-    WHERE name = ? LIMIT 1 
-    SQL 
+    WHERE name = ? 
+    LIMIT 1 
+    SQL
     DB[:conn].execute(sql, name).map do |row|
       self.new_from_db(row)
     end.first
