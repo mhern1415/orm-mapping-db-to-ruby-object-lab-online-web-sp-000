426e8a09213367e05a9da3839d58fbe51facbe40
Mark Hernandez <m.hern1415@gmail.com>
2019-09-03 23:19:13 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -83,7 +83,9 @@ class Student
   end
   
   def self.first_X_students_in_grade_10(x)
-    
+    sql = "SELECT * FROM students WHERE grade = 10 LIMIT ?"
+    DB[:conn].execute(sql, x)
+  end
     
   
   
